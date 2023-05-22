381b401fa1c24cfb381179355f2d529f613b5a80
kazimahin68 <kazimahin68@gmail.com>
2023-05-22 21:31:15 +0600

    search button added to all toy page

--- src/pages/AllToys/AllToys.jsx
+++ src/pages/AllToys/AllToys.jsx
@@ -4,17 +4,29 @@ import { useState } from "react";
 
 const AllToys = () => {
     const toys = useLoaderData();
+    const [toyToDisplay, setToyToDisplay] = useState(toys);
     const [displayToy, setDisplayToy] = useState(false)
     const handleShowAll = () => {
         setDisplayToy(!displayToy)
     }
+    const handleSearch = event =>{
+        event.preventDefault()
+        const searchedValue = event.target.search.value
+        const searchedItems = toys.filter(toy => toy.toyName.toLowerCase() === searchedValue.toLowerCase());
+        setToyToDisplay(searchedItems);
+    }
 
-    const allToys = [...toys];
+
+    const allToys = [...toyToDisplay];
     const shownToys = allToys.slice(0, 20);
-    const toysToSHow = displayToy ? toys : shownToys;
+    const toysToSHow = displayToy ? toyToDisplay : shownToys;
     return (
         <div>
             <div className="overflow-x-auto md:w-4/5 mx-auto mt-12">
+                <form onSubmit={handleSearch} className="mb-5 text-center">
+                    <input type="text" name="search" placeholder="Search by name" className="rounded-xl p-3"/>
+                    <input className="btn bg-gradient-to-r from-[#7E90FE] to-[#9873FF] border-none" type="submit" value="Search" />
+                </form>
                 <table className="table w-full table-zebra xl:table-fixed">
                     {/* head */}
                     <thead>
@@ -35,13 +47,12 @@ const AllToys = () => {
                             ></AllToyRow>)
                         }
                     </tbody>
-                    <tfoot className="w-full">
+                </table>
+                    <div className="w-full">
                         <div className='text-center bg-white rounded-lg p-2'>
                             <button onClick={handleShowAll} className='btn bg-gradient-to-r from-[#7E90FE] to-[#9873FF] border-none'>{displayToy ? "Show Less Toys" : "Show All Toys"}</button>
                         </div>
-
-                    </tfoot>
-                </table>
+                    </div>
             </div>
         </div>
     );
