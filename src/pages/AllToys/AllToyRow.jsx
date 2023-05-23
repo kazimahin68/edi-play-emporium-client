98befac9d0c3cabdc2576c420b5893d68458d9bd
kazimahin68 <kazimahin68@gmail.com>
2023-05-23 21:47:31 +0600

    blog page added

--- src/pages/AllToys/AllToyRow.jsx
+++ src/pages/AllToys/AllToyRow.jsx
@@ -1,7 +1,16 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../Provider/AuthProvider";
+import toast, { Toaster } from 'react-hot-toast';
 
 const AllToysRow = ({ toy, index }) => {
+    const {user} = useContext(AuthContext)
     const {_id, userName, toyName, subCategory, price, photo, quantity } = toy;
+    const handleAlert = () =>{
+        if(!user){
+            toast('You have to log in first to view details')
+        }
+    }
 
     return (
         <tr>
@@ -29,7 +38,8 @@ const AllToysRow = ({ toy, index }) => {
                 <p className="font-bold">{quantity}</p>
             </td>
             <td className="whitespace-normal">
-                <Link to={`/toy/${_id}`} className="btn font-bold bg-[#7c9c05] border-none hover:bg-[#a5c926]">View Details</Link>
+                <Link to={`/toy/${_id}`} onClick={handleAlert} className="btn font-bold bg-[#7c9c05] border-none hover:bg-[#a5c926]">View Details</Link>
+                <Toaster></Toaster>
             </td>
         </tr>
     );
--- src/pages/Blogs/Blogs.jsx
+++ src/pages/Blogs/Blogs.jsx
@@ -1,8 +1,118 @@
 
 const Blogs = () => {
     return (
-        <div>
-            <h2>Blogs.....</h2>
+        <div className="md:w-4/5 mx-auto mt-12 px-20 py-10 rounded-lg" style={{ backgroundImage: `url(https://i.ibb.co/qBP1Sf5/login-background.png)` }}>
+            <div>
+                <h2 className="font-extrabold text-4xl text-center mb-5 opacity-80">1. What is an access token and refresh token? How do they work and where should we store them on the client-side?</h2>
+
+                <h1 className="font-bold text-3xl opacity-80 mb-5">Access Token and Refresh Token</h1>
+
+                <h2 className="font-semibold text-2xl opacity-80">Access Token:</h2>
+                <p className="opacity-80 mb-5">An access token is a credential that is issued by an authentication server to a client application after the user successfully authenticates. It serves as proof of the users identity and authorization to access specific resources or perform certain actions. Access tokens are typically short-lived and have an expiration time.</p>
+
+                <h2 className="font-semibold text-2xl opacity-80">Refresh Token:</h2>
+                <p className="opacity-80 mb-5">A refresh token is also issued by the authentication server alongside the access token. Unlike access tokens, refresh tokens are long-lived and are used to obtain new access tokens when the current one expires. They allow the client application to request a new access token without requiring the user to re-authenticate. Refresh tokens have a longer expiration time and should be securely stored by the client application.</p>
+
+                <h2 className="font-semibold text-2xl opacity-80">Storage on the Client-side:</h2>
+                <p className="opacity-80 mb-5">When it comes to storing access and refresh tokens on the client-side, security is of utmost importance. Here are some recommendations:</p>
+
+                <h3 className="font-semibold text-2xl opacity-80">Access Token:</h3>
+                <p className="opacity-80 mb-5">The access token should be stored securely but readily accessible by the client application. It is commonly stored in memory or in a short-lived browser storage mechanism, such as the browsers memory or session storage. Storing it in a cookie with appropriate security measures (e.g., secure and HTTP-only flags) can also be an option, depending on your application is requirements.</p>
+
+                <h3 className="font-semibold text-2xl opacity-80">Refresh Token:</h3>
+                <p className="opacity-80 mb-5">The refresh token requires more stringent security measures since it has a longer lifespan and can be used to obtain new access tokens. It should be stored securely, preferably using an encrypted or hashed format, to protect against unauthorized access or theft. Common approaches include storing the refresh token in an HTTP-only cookie with the secure flag, or in a client-side storage mechanism such as localStorage or IndexedDB with appropriate security measures.</p>
+
+                <p className="opacity-80 mb-5">It is important to note that the specific storage mechanism and security measures may vary depending on the framework, programming language, and security requirements of your application. Always consult relevant security guidelines and best practices specific to your technology stack.</p>
+            </div>
+            <div className="mt-12">
+                <h2 className="font-extrabold text-4xl text-center mb-5 opacity-80">2. Compare SQL and NoSQL databases?</h2>
+                <h1 className="font-bold text-3xl opacity-80 mb-5">Comparison: SQL vs NoSQL Databases</h1>
+
+                <h2 className="font-semibold text-2xl opacity-80">Data Model:</h2>
+                <ul className="opacity-80 mb-5 list-disc">
+                    <li><strong>SQL:</strong> Structured data model based on tables with predefined schemas.</li>
+                    <li><strong>NoSQL:</strong> Various data models like key-value, document, columnar, or graph-based with flexible schemas.</li>
+                </ul>
+
+                <h2 className="font-semibold text-2xl opacity-80">Query Language:</h2>
+                <ul className="opacity-80 mb-5 list-disc">
+                    <li><strong>SQL:</strong> Uses SQL (Structured Query Language) for querying and managing data.</li>
+                    <li><strong>NoSQL:</strong> Different query languages based on data models (e.g., JSON-like queries for document databases).</li>
+                </ul>
+
+                <h2 className="font-semibold text-2xl opacity-80">Scalability:</h2>
+                <ul className="opacity-80 mb-5 list-disc">
+                    <li><strong>SQL:</strong> Vertical scalability by increasing server hardware resources.</li>
+                    <li><strong>NoSQL:</strong> Horizontal scalability by distributing data across multiple servers or clusters.</li>
+                </ul>
+
+                <h2 className="font-semibold text-2xl opacity-80">Schema Flexibility:</h2>
+                <ul className="opacity-80 mb-5 list-disc">
+                    <li><strong>SQL:</strong> Enforces rigid schemas, requiring predefined data structures.</li>
+                    <li><strong>NoSQL:</strong> Provides schema flexibility, allowing dynamic and evolving data models.</li>
+                </ul>
+
+                <h2 className="font-semibold text-2xl opacity-80">ACID Compliance:</h2>
+                <ul className="opacity-80 mb-5 list-disc">
+                    <li><strong>SQL:</strong> Provides strong ACID guarantees for data integrity and consistency.</li>
+                    <li><strong>NoSQL:</strong> May relax ACID guarantees to prioritize scalability and performance.</li>
+                </ul>
+
+                <p className="opacity-80 mb-5">It is important to consider project requirements when choosing between SQL and NoSQL databases. Factors such as data nature, scalability needs, development agility, and query complexity play a significant role in the decision-making process.</p>
+            </div>
+            <div className="mt-12">
+                <h2 className="font-extrabold text-4xl text-center mb-5 opacity-80">3. What is express js? What is Nest JS?</h2>
+                <h1 className="font-bold text-3xl opacity-80 mb-5">Express.js</h1>
+                <p className="opacity-80 mb-5">
+                    Express.js is a minimalistic and flexible web application framework for Node.js. It provides a set of features and utilities to build web applications and APIs. Express.js simplifies the process of creating server-side applications by offering a straightforward and unopinionated approach.
+                </p>
+                <h3 className="font-semibold text-2xl opacity-80">Key Features of Express.js:</h3>
+                <ul className="opacity-80 mb-5 list-disc">
+                    <li>Routing</li>
+                    <li>Middleware</li>
+                    <li>Templating Engines</li>
+                    <li>HTTP Methods</li>
+                    <li>Middleware Ecosystem</li>
+                </ul>
+
+                <h1 className="font-semibold text-2xl opacity-80">Nest.js</h1>
+                <p className="opacity-80 mb-5">
+                    Nest.js is a progressive, TypeScript-based web application framework built on top of Node.js. It provides a scalable and maintainable architecture for building server-side applications. Nest.js draws inspiration from various frameworks and patterns such as Express.js, Angular, and object-oriented programming.
+                </p>
+                <h3 className="font-semibold text-2xl opacity-80">Key Features of Nest.js:</h3>
+                <ul className="opacity-80 mb-5 list-disc">
+                    <li>Modular and Hierarchical Structure</li>
+                    <li>Decorators and TypeScript</li>
+                    <li>Dependency Injection</li>
+                    <li>Middleware and Interceptors</li>
+                    <li>Database Integration</li>
+                    <li>Testing</li>
+                </ul>
+
+                <p className="opacity-80 mb-5">
+                    Both Express.js and Nest.js are powerful frameworks for building server-side applications, but they differ in their design philosophy, architecture, and features. Developers can choose the framework that best suits their project requirements and personal preferences.
+                </p>
+            </div>
+            <div>
+                <h2 className="font-extrabold text-4xl text-center mb-5 opacity-80">4. What is MongoDB aggregate and how does it work?</h2>
+                <h1 className="font-bold text-3xl opacity-80 mb-5">MongoDB Aggregate</h1>
+                <p className="opacity-80 mb-5">
+                    MongoDBs aggregate is a powerful method used for performing advanced data analysis and transformation operations on collections. It allows you to process and combine documents in a collection to generate aggregated results based on a defined set of stages.
+                </p>
+                <h2 className="font-semibold text-2xl opacity-80 mb-5">How MongoDB Aggregate Works:</h2>
+                <ol className="list-decimal mb-5 opacity-80">
+                    <li className="mb-2"><strong>Pipeline Stages:</strong> The aggregate method takes an array of pipeline stages as its argument. Each stage represents a specific operation or transformation to be applied to the documents in the collection. The stages are processed in the order they appear in the array.</li>
+                    <li className="mb-2"><strong>Pipeline Operators:</strong> Within each stage, you can use a variety of pipeline operators to perform specific actions on the data. Some common pipeline operators include $match, $group, $project, $sort, $limit, $skip, and $unwind.</li>
+                    <li className="mb-2"><strong>Chaining Pipeline Stages:</strong> You can chain multiple stages together to build complex data processing pipelines. Each stage takes the output of the previous stage as its input, allowing you to progressively transform and aggregate the data.</li>
+                    <li className="mb-2"><strong>Aggregated Results:</strong> After all the stages in the pipeline are processed, the aggregate method returns the final aggregated results. The results can include statistics, calculations, grouped data, or any other transformation performed throughout the pipeline.</li>
+                </ol>
+                <p className="opacity-80 mb-5">
+                    By using the aggregate method, you can perform various data manipulations, such as filtering, grouping, sorting, projecting specific fields, and calculating aggregations like sums, averages, maximums, minimums, and more. This allows you to perform advanced data analysis and generate customized reports from your MongoDB collections.
+                </p>
+                <p className="opacity-80 mb-5">
+                    It is worth noting that constructing effective pipelines and understanding the different pipeline stages and operators requires a good understanding of the data and the desired output. MongoDBs official documentation provides detailed information on aggregate usage and various pipeline stages and operators to help you leverage the full power of aggregation in MongoDB.
+                </p>
+            </div>
         </div>
     );
 };
